64b3842a44f2853c325914fefdaa789940db64e2
wmbnbc <wesblue@gmail.com>
2017-09-29 17:56:24 -0400

    Add files via upload

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formSentry
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.buttonAcademic = New System.Windows.Forms.Button()
+        Me.buttonPersonal = New System.Windows.Forms.Button()
+        Me.labelWelcome = New System.Windows.Forms.Label()
+        Me.labelTypeOfWork = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'buttonAcademic
+        '
+        Me.buttonAcademic.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.buttonAcademic.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonAcademic.Location = New System.Drawing.Point(34, 275)
+        Me.buttonAcademic.Name = "buttonAcademic"
+        Me.buttonAcademic.Size = New System.Drawing.Size(255, 85)
+        Me.buttonAcademic.TabIndex = 0
+        Me.buttonAcademic.Text = "Academic"
+        Me.buttonAcademic.UseVisualStyleBackColor = True
+        '
+        'buttonPersonal
+        '
+        Me.buttonPersonal.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.buttonPersonal.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonPersonal.Location = New System.Drawing.Point(338, 275)
+        Me.buttonPersonal.Name = "buttonPersonal"
+        Me.buttonPersonal.Size = New System.Drawing.Size(255, 85)
+        Me.buttonPersonal.TabIndex = 1
+        Me.buttonPersonal.Text = "Personal"
+        Me.buttonPersonal.UseVisualStyleBackColor = True
+        '
+        'labelWelcome
+        '
+        Me.labelWelcome.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.labelWelcome.AutoSize = True
+        Me.labelWelcome.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelWelcome.Location = New System.Drawing.Point(15, 80)
+        Me.labelWelcome.Name = "labelWelcome"
+        Me.labelWelcome.Size = New System.Drawing.Size(606, 37)
+        Me.labelWelcome.TabIndex = 2
+        Me.labelWelcome.Text = "Welcome to the NTID Learning Center!"
+        '
+        'labelTypeOfWork
+        '
+        Me.labelTypeOfWork.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.labelTypeOfWork.AutoSize = True
+        Me.labelTypeOfWork.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelTypeOfWork.Location = New System.Drawing.Point(87, 191)
+        Me.labelTypeOfWork.Name = "labelTypeOfWork"
+        Me.labelTypeOfWork.Size = New System.Drawing.Size(452, 31)
+        Me.labelTypeOfWork.TabIndex = 3
+        Me.labelTypeOfWork.Text = "What type of work will you be doing?"
+        '
+        'formSentry
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 419)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.labelTypeOfWork)
+        Me.Controls.Add(Me.labelWelcome)
+        Me.Controls.Add(Me.buttonPersonal)
+        Me.Controls.Add(Me.buttonAcademic)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "formSentry"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "NLC Sentry"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents buttonAcademic As Button
+    Friend WithEvents buttonPersonal As Button
+    Friend WithEvents labelWelcome As Label
+    Friend WithEvents labelTypeOfWork As Label
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,227 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.IO
+Imports System.Text
+Imports System.Runtime.InteropServices
+Imports System.Net
+Imports System.Xml
+
+Public Class formSentry
+
+    Private Declare Function GetLastInputInfo Lib "User32.dll" (ByRef lastInput As LASTINPUTINFO) As Boolean
+
+    <StructLayout(LayoutKind.Sequential)> Public Structure LASTINPUTINFO
+        Public cbSize As Int32
+        Public dwTime As Int32
+    End Structure
+
+    Dim hostName As String = Environment.MachineName
+    Dim userName As String = Environment.UserName
+    Dim typePersonal As String = "Personal"
+    Dim typeAcademic As String = "Academic"
+    Dim startTime = DateTime.Now
+    Dim addComma As String = ","
+
+    Dim startUpWriteMsg As String = hostName & addComma & userName & addComma & typePersonal & addComma & startTime.ToString()
+    Dim academicWriteMsg As String = hostName & addComma & userName & addComma & typeAcademic & addComma & startTime.ToString()
+
+    Dim homePath As String = "\\NISSRV5\NLCLabManager$\Sentry\"
+    Dim csvPath As String = hostName & "\" & hostName & ".csv"
+    Dim shutdownFilePath As String = homePath & hostName & "\shutdown.txt"
+
+    Dim pauseTime As Integer = 1000 * 60
+
+    Dim shutdownThread As New System.Threading.Thread(AddressOf keepShutdownFileActive)
+    Dim idleThread As New System.Threading.Thread(AddressOf idleDetector)
+
+    Dim newInfo As String = "1"
+    Dim updateInfo As String = "3"
+    Dim closeInfo As String = "0"
+
+    Dim urlPath As String = "https://www.ntid.rit.edu/nlc/sentry?"
+    Dim newUrl As String = urlPath & "&host=" & hostName & "&user=" & userName & "&priority=" & typePersonal & "&status=" & newInfo
+    Dim updateUrl As String = urlPath & "&host=" & hostName & "&status=" & updateInfo
+    Dim closeUrl As String = urlPath & "&host=" & hostName & "&status=" & closeInfo
+
+    Private Sub formSentry_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        'MessageBox.Show(homePath & csvPath)
+        directoryDuty(homePath, 1)
+        writeCSV(homePath & csvPath, 1)
+        sendUrl(1)
+
+        shutdownThread.Start()
+        idleThread.Start()
+
+        'testXML()
+
+    End Sub
+
+    Private Sub buttonAcademic_Click(sender As Object, e As EventArgs) Handles buttonAcademic.Click
+
+        writeCSV(homePath & csvPath, 3)
+        sendUrl(3)
+
+        'Me.Hide()
+
+
+    End Sub
+
+    Private Sub buttonPersonal_Click(sender As Object, e As EventArgs) Handles buttonPersonal.Click
+
+        'Me.Hide()
+
+    End Sub
+
+    Private Sub directoryDuty(path As String, action As Integer)
+
+        Select Case action
+            Case 0
+                My.Computer.FileSystem.DeleteFile(shutdownFilePath)
+            Case 1
+                If Not Directory.Exists(path) Then
+                    My.Computer.FileSystem.CreateDirectory(homePath)
+                    createFile(homePath & csvPath)
+                Else
+                    If Not File.Exists(path & csvPath) Then
+                        createFile(homePath & csvPath)
+                    End If
+                End If
+
+                If Not File.Exists(shutdownFilePath) Then
+                    createFile(shutdownFilePath)
+                Else
+                    writeCSV(homePath & csvPath, 0)
+                    My.Computer.FileSystem.DeleteFile(shutdownFilePath)
+                    createFile(shutdownFilePath)
+                End If
+        End Select
+
+
+    End Sub
+
+    Private Sub createFile(path As String)
+
+        Dim fs As FileStream = File.Create(path)
+        fs.Close()
+
+    End Sub
+
+    Private Sub deleteFile(path As String)
+
+    End Sub
+
+    Private Sub writeCSV(path As String, action As Integer)
+
+        Select Case action
+            Case 0
+
+                Dim endTime = File.GetLastWriteTime(shutdownFilePath)
+
+                My.Computer.FileSystem.WriteAllText(path, addComma & endTime.ToString() & vbCrLf, True)
+
+            Case 1
+
+                My.Computer.FileSystem.WriteAllText(path, startUpWriteMsg, True)
+
+            Case 3
+
+                Dim arrText() As String = File.ReadAllLines(homePath & csvPath)
+
+                My.Computer.FileSystem.DeleteFile(homePath & csvPath)
+                createFile(homePath & csvPath)
+
+                Using objWriter As StreamWriter = New StreamWriter(homePath & csvPath)
+                    For I As Integer = 1 To arrText.Length - 1
+                        objWriter.WriteLine(arrText(I))
+                    Next
+                End Using
+
+                My.Computer.FileSystem.WriteAllText(path, academicWriteMsg, True)
+
+        End Select
+
+
+    End Sub
+
+    Private Sub idleDetector()
+
+        Do While True
+            Dim lastInput As New LASTINPUTINFO
+
+            lastInput.cbSize = Marshal.SizeOf(lastInput)
+
+            If GetLastInputInfo(lastInput) Then
+                'MessageBox.Show((Environment.TickCount - lastInput.dwTime) / 1000)
+                If ((Environment.TickCount - lastInput.dwTime) / 1000) > 600 Then
+                    'MessageBox.Show("Idle for 10 minutes!")
+                End If
+            End If
+
+            System.Threading.Thread.Sleep(pauseTime)
+        Loop
+
+    End Sub
+
+    Private Sub keepShutdownFileActive()
+
+        Do While True
+
+            My.Computer.FileSystem.WriteAllText(shutdownFilePath, " ", True)
+            System.Threading.Thread.Sleep(pauseTime)
+
+        Loop
+
+    End Sub
+
+    Private Sub sendUrl(action As Integer)
+
+        Dim ie As Object
+        ie = CreateObject("InternetExplorer.Application")
+
+        Select Case action
+            Case 0 'Closing
+                ie.navigate(closeUrl)
+            Case 1 'New
+                ie.navigate(newUrl)
+            Case 3 'Update Priority
+                ie.navigate(updateUrl)
+        End Select
+
+    End Sub
+
+    Private Sub formSentry_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+
+        writeCSV(homePath & csvPath, 0)
+        directoryDuty(shutdownFilePath, 0)
+        sendUrl(0)
+
+        shutdownThread.Abort()
+        idleThread.Abort()
+
+    End Sub
+
+    Sub testXML()
+
+        Dim reader As XmlTextReader = New XmlTextReader("\\NISSRV5\NLCLabManager$\Sentry\survey.xml")
+
+        Do While (reader.Read())
+            Select Case reader.NodeType
+                Case XmlNodeType.Element 'Display beginning of element.
+                    MessageBox.Show(reader.Name)
+                    If reader.HasAttributes Then 'If attributes exist
+                        While reader.MoveToNextAttribute()
+                            'Display attribute name and value.
+                            MessageBox.Show(reader.Name & " " & reader.Value)
+                        End While
+                    End If
+                Case XmlNodeType.Text 'Display the text in each element.
+                    MessageBox.Show(reader.Value)
+                Case XmlNodeType.EndElement 'Display end of element.
+                    MessageBox.Show(reader.Name)
+            End Select
+        Loop
+
+    End Sub
+
+End Class
